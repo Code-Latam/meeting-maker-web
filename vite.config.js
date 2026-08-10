9f5d32ca727ccb19a2f77e4306445b1d0b8a9982
stevenJonathanB <119703436+stevenJonathanB@users.noreply.github.com>
2026-08-10 18:01:03 -0500

    fixes
    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  }
 })
